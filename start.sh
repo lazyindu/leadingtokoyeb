579479a28687334913a7c055bb70f8264f6b9b2c
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-02 17:03:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/lazyindu/mv2.git /Mdisk-Search-Bot-V2
+git clone https://github.com/lazyindu/LazyUrlHunterrBOT.git /Mdisk-Search-Bot-V2
 cd /Mdisk-Search-Bot-V2
 pip3 install -r requirements.txt
-echo "Starting Bot....ANURAG"
+echo "Starting Bot..."
 python3 main.py
